764060a5e211e73fda5f868d7099da8ebfbb3850
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2025-12-09 09:48:58 +0530

    Initial commit: Angular SSR project setup and fixes

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,9 @@
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
+// Export a default function that takes a context argument for SSR
+export default function bootstrap(context: any) {
+  return bootstrapApplication(AppComponent, config, context);
+}
